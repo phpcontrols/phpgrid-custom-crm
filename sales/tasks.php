5e896d1a9354e995ef84b38e2181d7adb3d2dedf
phpcontrols <richard@phpcontrols.com>
2017-01-17 11:46:20 +0800

    change from dashboard to tasks

--- sales/tasks.php
+++ sales/tasks.php
@@ -9,7 +9,7 @@ include_once('../inc/head.php');
 <h1>Custom CRM</h1>
 
 <?php
-$_GET['currentPage'] = 'dashboard';
+$_GET['currentPage'] = 'tasks';
 include_once('../inc/menu.php');
 ?>
 
@@ -22,13 +22,15 @@ Choose an option from the menu above or view your current tasks below.
 
 <h3>My Current Tasks</h2>
 <?php
-$dg = new C_DataGrid("SELECT ID, `Date`, contact, todo_type_id, todo_desc_id, Task_Update,  Todo_Due_Date FROM notes", "ID", "notes");
+$dg = new C_DataGrid("SELECT ID, `Date`, contact, todo_type_id, todo_desc_id, task_status, Task_Update, sales_rep, Todo_Due_Date FROM notes", "ID", "notes");
+$dg->set_query_filter(" sales_rep = 1 && task_status != 2");
 
-$dg->set_col_hidden('ID')->set_caption(' ');
+$dg->set_col_hidden('ID')->set_col_hidden('sales_rep', false)->set_caption(' ');
 
 $dg->set_col_title('todo_type_id', 'Todo Type');
 $dg->set_col_title('todo_desc_id', 'Description');
 
+$dg->set_col_edittype('task_status', 'select', 'SELECT ID, status FROM task_status');
 $dg->set_col_edittype('contact', 'select', 'SELECT ID, Contact_Last FROM Contact');
 $dg->set_col_edittype('todo_type_id', 'select', 'SELECT ID, Type FROM todo_type');
 $dg->set_col_edittype('todo_desc_id', 'select', 'SELECT ID, Description FROM todo_desc');
