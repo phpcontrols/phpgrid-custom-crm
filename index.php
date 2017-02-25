c76651d1474b8b499d38586766baec8835f2eb52
phpcontrols <richard@phpcontrols.com>
2017-02-25 12:06:56 +0800

    Update index.php to add context

--- index.php
+++ index.php
@@ -9,7 +9,35 @@
 <h1>PHP FOR BEGINNERS: BUILD YOUR FIRST SIMPLE CRM FROM SCRATCH</h1>
 
 <div>
-    Demo:
+<p>
+Customer Relationship Management (CRM) is a system that manages customer interactions and data throughout the customer lifecycle between the customer and the company across different channels. 
+</p>
+
+<p>
+In the simple CRM system demo, salespeople can:
+</p>
+
+<ul>
+<li>Access their tasks</li>
+<li>View their leads </li>
+<li>Create new tasks for each lead</li>
+<li>Create new opportunity</li>
+<li>Lose a sale</li>
+</ul>
+
+<p>
+Sales managers will be able to:
+</p>
+
+<ul>
+<li>Manage all customers </li>
+<li>Manage sales team</li>
+<li>View current sales activities</li>
+</ul>
+</div>
+
+<div>
+    <h2>Visit Demo:</h2>
     <div>   
         <a href="sales/tasks.php">Sales</a> | <a href="managers/pipeline.php">Manager</a>
     </div>
